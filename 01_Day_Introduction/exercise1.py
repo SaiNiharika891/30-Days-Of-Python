518a25967fd8073d87132a2c88d70e11c6cb1188
SaiNiharika891 <52854352+SaiNiharika891@users.noreply.github.com>
2022-05-16 12:45:53 +0530

    exercise 1 day1
    I have learned data types of python
    and completed some tasks in exercise1

---
+++ 01_Day_Introduction/exercise1.py
@@ -0,0 +1,48 @@
+from pickle import FALSE
+from xmlrpc.client import Boolean
+
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+print("niha")
+print("Gude")
+print("india")
+print("I am enjoying 30 days of python")
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("niha"))
+print(type("Gude"))
+print(type("india"))
+numeric = 2
+print(numeric, "is of type", type(numeric))
+float = 1.5
+print(float, "is of type", type(float))
+complex = 8+3j
+print(complex, "is a type", type(complex))
+String1 = "Hello"
+String2 ="World"
+print(String1+String2)
+list  = [1, "hi", "Python", 2]    
+print(type(list))  
+Set = {4,3,6.6,"Hello"}
+print(Set)
+t  = ("hi", "Python", 2)    
+print (type(t))
+dict = {1:'Jimmy', 2:'Alex', 3:'john', 4:'mike'}
+print(dict)
+bool = True , False
+print(type(bool))
+#Euclidian distance 
+import math 
+p1 = [2, 3]
+p2 = [10, 8]
+distance = math.sqrt( ((p1[2]-p2[3])**2)+((p1[10]-p2[8])**2) )
+print(distance)
